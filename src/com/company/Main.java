9b65d9e2d89116f7d9065fd20c339d0cdb4bd6f6
jrmensah <jerilynmensah@gmail.com>
2017-10-11 15:11:10 -0400

    Printing my name and age

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Print my name and age
+
+        System.out.println("My name is Jerilyn Mensah");
+        System.out.println("I am 30 years old");
+    }
+}
